1761e878e4bf7bdd4a63932c9bd1fcf71beb00f1
wuyajun <yajun.wu@niuniuqiche.com>
2017-07-06 21:37:13 +0800

    add code

--- CarComparison/app/src/main/java/cn/peter/carcomparison/vhtableview/HListViewScrollView.java
+++ CarComparison/app/src/main/java/cn/peter/carcomparison/vhtableview/HListViewScrollView.java
@@ -7,49 +7,49 @@ import android.widget.HorizontalScrollView;
 
 public class HListViewScrollView extends HorizontalScrollView {
 
+    public HListViewScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
 
-	public HListViewScrollView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
+    public HListViewScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	
-	public HListViewScrollView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public HListViewScrollView(Context context) {
+        super(context);
+    }
 
-	public HListViewScrollView(Context context) {
-		super(context);
-	}
-	
-	@Override
-	public boolean onTouchEvent(MotionEvent ev) {
-		//记录当前触摸的HListViewScrollView
-		if(null!=this.listener){
-			listener.setCurrentTouchView(this);
-		}
-		return super.onTouchEvent(ev);
-	}
-	
-	@Override
-	protected void onScrollChanged(int l, int t, int oldl, int oldt) {
-		//当当前的HListViewScrollView被触摸时，滑动其它
-		if(null!=this.listener&&null!=listener.getCurrentTouchView()&&listener.getCurrentTouchView() == this) {
-			listener.onUIScrollChanged(l, t, oldl, oldt);
-		}else{
-			super.onScrollChanged(l, t, oldl, oldt);
-		}
-	}
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        //记录当前触摸的HListViewScrollView
+        if (null != this.listener) {
+            listener.setCurrentTouchView(this);
+        }
+        return super.onTouchEvent(ev);
+    }
 
-	private ScrollChangedListener listener;
-	public void setScrollChangedListener(ScrollChangedListener listener){
-		this.listener=listener;
-	}
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        //当当前的HListViewScrollView被触摸时，滑动其它
+        if (null != this.listener && null != listener.getCurrentTouchView() && listener.getCurrentTouchView() == this) {
+            listener.onUIScrollChanged(l, t, oldl, oldt);
+        } else {
+            super.onScrollChanged(l, t, oldl, oldt);
+        }
+    }
 
-	public interface ScrollChangedListener{
+    private ScrollChangedListener listener;
 
-		public void setCurrentTouchView(HListViewScrollView currentTouchView);
+    public void setScrollChangedListener(ScrollChangedListener listener) {
+        this.listener = listener;
+    }
 
-		public HListViewScrollView getCurrentTouchView();
-		public void onUIScrollChanged(int l, int t, int oldl, int oldt);
-	}
+    public interface ScrollChangedListener {
+
+        void setCurrentTouchView(HListViewScrollView currentTouchView);
+
+        HListViewScrollView getCurrentTouchView();
+
+        void onUIScrollChanged(int l, int t, int oldl, int oldt);
+    }
 }
